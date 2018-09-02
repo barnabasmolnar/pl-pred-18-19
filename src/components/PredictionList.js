716c190b178fbea00b956c4056dc9b15c57c092f
Barnabás Molnár <barna.molnar90@gmail.com>
2018-09-02 21:40:48 +0200

    Sorting functionality halfway done.

--- src/components/PredictionList.js
+++ src/components/PredictionList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from "axios";
 import PredictionListItem from "./PredictionListItem";
+import classnames from "classnames";
 
 class PredictionList extends Component {
     constructor(props) {
@@ -11,8 +12,46 @@ class PredictionList extends Component {
             .catch(err => console.log(err))
 
         this.state = {
-            predictions: []
+            predictions: [],
+            sortState: {
+                prop: "dateSent",
+                isDesc: true
+            }
         }
+
+        this.handleUserNameSort = this.handleUserNameSort.bind(this);
+        this.handleDateSentSort = this.handleDateSentSort.bind(this);
+    }
+
+    handleUserNameSort() {
+        this.setState(prevState =>
+            (
+                {
+                    sortState: {
+                        prop: "userName",
+                        isDesc: !prevState.sortState.isDesc
+                    },
+                    predictions: prevState.predictions.slice().sort((a, b) =>
+                        {
+                            if (!prevState.sortState.isDesc) {
+                                [a, b] = [b, a];
+                            }
+                            
+                            return a.userName.localeCompare(b.userName);
+                        }
+                    )
+                }
+            )
+        )
+    }
+
+    handleDateSentSort() {
+        this.setState(prevState => 
+            ({sortState: {
+                prop: "dateSent",
+                isDesc: !prevState.sortState.isDesc
+            }})
+        )
     }
 
     render() {
@@ -35,6 +74,38 @@ class PredictionList extends Component {
                             everyone's predictions. Just click or tap on one of the list items below and you will be taken
                             to that user's predictions.
                         </p>
+                    </div>
+                </div>
+
+                <div className="container mx-auto my-4">
+                    <div className="bg-purple-lightest p-4 text-grey-darker">
+                        <button
+                            className={ classnames("btn btn-secondary inline-block align-middle m-2", {"btn-inactive": this.state.sortState.prop !== "userName"}) }
+                            onClick={this.handleUserNameSort}
+                        >
+                            <span className="inline-block align-bottom">Sort by username</span>
+                            <svg
+                                className={ classnames("w-4 ml-2 fill-current text-pink-dark inline-block align-bottom", {"invisible": this.state.sortState.prop !== "userName", "icon--desc": this.state.sortState.isDesc}) }
+                                version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 20 20" enableBackground="new 0 0 20 20"
+                            >
+                                <path d="M17,1h-2c-0.552,0-1,0.447-1,1v16.992h4V2C18,1.447,17.553,1,17,1z M11,7H9C8.448,7,8,7.447,8,8v10.992h4V8
+                                    C12,7.447,11.553,7,11,7z M5,13H3c-0.552,0-1,0.447-1,1v4.992h4V14C6,13.447,5.553,13,5,13z"/>
+                            </svg>
+                        </button>
+
+                        <button
+                            className={ classnames("btn btn-secondary inline-block align-middle m-2", {"btn-inactive": this.state.sortState.prop !== "dateSent"}) }
+                            onClick={this.handleDateSentSort}
+                        >
+                            <span className="inline-block align-bottom">Sort by date</span>
+                            <svg
+                                className={ classnames("w-4 ml-2 fill-current text-pink-dark align-bottom", {"invisible": this.state.sortState.prop !== "dateSent", "icon--desc": this.state.sortState.isDesc}) }
+                                version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 20 20" enableBackground="new 0 0 20 20"
+                            >
+                                <path d="M17,1h-2c-0.552,0-1,0.447-1,1v16.992h4V2C18,1.447,17.553,1,17,1z M11,7H9C8.448,7,8,7.447,8,8v10.992h4V8
+                                    C12,7.447,11.553,7,11,7z M5,13H3c-0.552,0-1,0.447-1,1v4.992h4V14C6,13.447,5.553,13,5,13z"/>
+                            </svg>
+                        </button>
                     </div>
                 </div>
 
